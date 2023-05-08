08ab8dabccf3af1c8db7756c28181929501f2ebc
jhonf1084 <johfredy1084@gmail.com>
2023-05-08 16:18:03 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  reset ():void{
+    this.counter = 10;
+  }
+
+}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  public characterList: Character[]=[{
+
+    name: 'Trunks',
+    power: 10
+
+  }]
+
+}
